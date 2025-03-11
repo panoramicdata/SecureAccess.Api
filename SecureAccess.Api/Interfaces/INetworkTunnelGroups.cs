aaa817b24fa4e3ef53a5cdc4519357067bd0e767
Peter Acs <peter.acs@panoramicdata.com>
2025-03-11 10:23:01 +0100

    Adjusted ListNetworkTunnelGroups params

--- SecureAccess.Api/Interfaces/INetworkTunnelGroups.cs
+++ SecureAccess.Api/Interfaces/INetworkTunnelGroups.cs
@@ -11,21 +11,21 @@ public interface INetworkTunnelGroups
 	/// If you enable the includeStatuses query parameter on your API request, then the status and tunnelsStatus fields are included in the response.
 	/// </summary>
 	/// <param name="filters"></param>
-	/// <param name="offset"></param>
-	/// <param name="limit"></param>
-	/// <param name="sortBy"></param>
-	/// <param name="sortOrder"></param>
-	/// <param name="includeStatuses"></param>
+	/// <param name="offset">An integer that represents the place to start reading in the collection. When the offset is 0, the first page is returned from the collection. If the limit is 10, the offset for the next page is 10. The default value is 0.</param>
+	/// <param name="limit">An integer that represents the number of records to return in the response. The default value is 10.</param>
+	/// <param name="sortBy">Specify the field that will be used to sort the items from the collection in the response. default = "name", enum = ["name", "status", "createdAt", "modifiedAt"]</param>
+	/// <param name="sortOrder">Specify the sort order (ascending or descending) for the items in the response. example = "asc", default = "asc", enum = ["asc", "desc"]</param>
+	/// <param name="includeStatuses">Specify whether to include the IPsec tunnel status fields (status and tunnelsStatus) for each hub. example = true, default = false</param>
 	/// <returns></returns>
 	[ApiOperationId("listNetworkTunnelGroups")]
 	[Get("/networktunnelgroups")]
 	Task<NetworkTunnelGroupsList> ListNetworkTunnelGroups(
-		[Query] string filters,
-		[Query] int offset = 0,
-		[Query] int limit = 10,
-		[Query] string sortBy = "name",
-		[Query] string sortOrder = "asc",
-		[Query] bool includeStatuses = false);
+	[Query] string? filters,
+	[Query] int? offset,
+	[Query] int? limit,
+	[Query] string? sortBy,
+	[Query] string? sortOrder,
+	[Query] bool? includeStatuses);
 
 	[ApiOperationId("addNetworkTunnelGroup")]
 	[Post("/networktunnelgroups")]
